5baa6c0d96d71c2a27dc478f3196a51e44bb33fc
Jared Sunday <jaredpatterson07@gmail.com>
2023-02-03 20:11:27 -0800

    changed algos

--- algos/game.js
+++ algos/game.js
@@ -12,7 +12,7 @@ var theDojo = [ [1, 0, 1, 1, 1, 0, 4, 0, 8, 0],
   var xBox
   var yBox
 
-// Creates the rows of buttons for this game
+
 function render(theDojo) {
   var result = [];
   for(var x=0; x<theDojo.length; x++) {
@@ -24,9 +24,7 @@ function render(theDojo) {
   }
   return result;
 }
-// TODO - Make this function tell us how many ninjas are hiding 
-//        under the adjacent (all sides and corners) squares.
-//        Use x and y as the indexes to check theDojo.
+
 function howMany(x, y, element) {
   tl = theDojo[y-1]
       ?theDojo[y-1][x-1]
@@ -68,23 +66,10 @@ function howMany(x, y, element) {
   element.innerText=totalNinja
   } 
 
-  // alert("TODO - determine how many ninjas are hiding in adjacent squares");
-  
-
-    
-// BONUS CHALLENGES
-// 1. draw the number onto the button instead of alerting it
-// 2. at the start randomly place 10 ninjas into theDojo with at most 1 on each spot
-// 3. if you click on a ninja you must restart the game 
-//    dojoDiv.innerHTML = `<button onclick="location.reload()">restart</button>`;
-    
-// start the game
-// message to greet a user of the game
 var style="color:cyan;font-size:1.5rem;font-weight:bold;";
 console.log("%c" + "IF YOU ARE A DOJO STUDENT...", style);
 console.log("%c" + "GOOD LUCK THIS IS A CHALLENGE!", style);
-// shows the dojo for debugging purposes
 console.table(theDojo);
-// adds the rows of buttons into <div id="the-dojo"></div> 
+ 
 dojoDiv.innerHTML = render(theDojo);    
 
